bffbfae5e909df9135e31ef86120974c037d859c
alvarorestrepo <alvarorestrepoz@gmail.com>
2020-05-06 18:11:44 -0500

    Se termino el link de Svelte y la aplicacion solo falta hacer ajustes de diseño

--- src/pages/_componentes/Textosvelte.svelte
+++ src/pages/_componentes/Textosvelte.svelte
@@ -1,6 +1,6 @@
 <script>
-import {Col, Row, Container} from 'sveltestrap'
-let imgan = '/img/reactp.png'
+  import { Col, Row, Container } from "sveltestrap";
+  let imgan = "/img/sveltep.png";
 </script>
 
 <style>
@@ -8,15 +8,17 @@ let imgan = '/img/reactp.png'
     text-align: justify;
     font-size: 1.3rem;
   }
-
+  li {
+    font-size: 1.2rem;
+    text-align: justify;
+    margin-top: 5px;
+  }
   .titulo1 {
     font-size: 3.5rem;
   }
 </style>
 
 <br />
-
-
 
 <Container>
 
@@ -27,15 +29,13 @@ let imgan = '/img/reactp.png'
     </Col>
     <Col xs="12" sm="6">
       <p>
-        Es un lenguaje ligero e interpretado, orientado a objetos con funciones
-        de primera clase, más conocido como el lenguaje de script para páginas
-        web, pero también usado en muchos entornos sin navegador, tales como
-        node.js, Apache CouchDB y Adobe Acrobat. Es un lenguaje script
-        multi-paradigma, basado en prototipos, dinámico, soporta estilos de
-        programación funcional, orientada a objetos e imperativa. Esta sección
-        de la web está dedicada netamente al lenguaje JavaScript, y no a las
-        partes que son específicas de las páginas web u otros entornos de
-        servidor.
+        SvelteJS es el nuevo chico del barrio que empieza a tener algo de
+        atención. Algunos pueden conocerlo por ser el lenguaje más popular para
+        State of JavaScript en 2018. Como explicación resumida, Svelte quiere
+        ser el framework que en realidad no es un framework; es básicamente una
+        herramienta para compilar componentes el momento de la construcción
+        (build), permitiéndote cargar un único bundle.js en tu página para
+        renderizar la aplicación.
       </p>
     </Col>
 
@@ -44,14 +44,8 @@ let imgan = '/img/reactp.png'
   <Row>
     <Col>
       <p>
-        Desde el 2012, todos los navegadores modernos soportan completamente
-        ECMAScript 5.1. Los navegadores más antiguos soportan por lo menos
-        ECMAScript 3. El 17 de Julio de 2015, ECMA International publicó la
-        sexta versión de ECMAScript, la cual es oficialmente llamada ECMAScript
-        2015, y fue inicialmente nombrada como ECMAScript 6 o ES6. Desde
-        entonces, los estándares ECMAScript están en ciclos de lanzamiento
-        anuales. Esta documentación se reere a la última versión del borrador,
-        que actualmente es ECMAScript 2019.
+        Esto significa que no hay DOM virtual, ni frameworks encima de
+        frameworks, no framework que cargar en tiempo de ejecución.
       </p>
     </Col>
 
@@ -59,9 +53,11 @@ let imgan = '/img/reactp.png'
   <Row>
     <Col>
       <p>
-        JavaScript no debe ser confundido con el lenguaje de programación Java.
-        Ambos "Java" y "Javascript" son marcas registradas de Oracle en Estados
-        Unidos y otros países. Sin embargo, los dos lenguajes de programa
+        Estos puntos son bastante atractivos para desarrolladores
+        experimentados, pero los principiantes posiblemente no puedan leer este
+        último párrafo sin que les explote la cabeza. Afortunadamente, no es
+        está compilación ‘mágica’ lo que hace que Svelte sea atractivo para los
+        principiantes, sino su sintaxis.
       </p>
     </Col>
   </Row>
@@ -70,18 +66,66 @@ let imgan = '/img/reactp.png'
 
 <Container>
   <h1 class="titulo1" align="center">
-    <strong>¿Qué es un framework de JavaScript?</strong>
+    <strong>De donde salio Svelte?</strong>
   </h1>
 </Container>
 <Container>
   <Row>
     <Col>
       <p>
-        En modo general, los frameworks o marcos de trabajo, son proyectos que
-        aglutinan bibliotecas de código y patrones de diseño avanzados, y sirven
-        como base para el desarrollo de aplicaciones diversas. En el caso de los
-        frameworks Javascript, sirven para el desarrollo de la parte frontend de
-        una web.
+        Svelte fue creado por Rich Harris, quien es desarrollador y que por
+        mucho tiempo buscaba una forma de mejorar el desempeño de las
+        aplicaciones que desarrollaba, él trabaja cumpliendo ciertas
+        características:
+      </p>
+    </Col>
+  </Row>
+
+  <Row>
+    <Col>
+      <ul>
+        <li>
+          Entregar productos con un excelente rendimiento y fácil desarrollo.
+        </li>
+        <li>
+          Que la librería o framework tuviera una curva de aprendizaje muy corta
+          para que otras personas puedan integrarlo en sus proyectos sin mucha
+          complicación.
+        </li>
+        <li>
+          Hacer más liviano el trabajo del navegador del lado cliente y reducir
+          lo más posible la carga de código de librerías o frameworks por código
+          JavaScript puro.
+        </li>
+      </ul>
+    </Col>
+  </Row>
+
+  <Row>
+    <Col>
+      <p>
+        El man en pocas palabras, se hartó y creó su propio JS framework a su
+        gusto. Luego de haber mencionado las intenciones de Rich harris,
+        entonces ya te preguntarás, muy bonita la historia pero ¿qué rayos y
+        centellas es Svelte? , a eso vamos.
+      </p>
+    </Col>
+  </Row>
+
+</Container>
+
+<Container>
+  <h1 class="titulo1" align="center">
+    <strong>Que es Svelte?</strong>
+  </h1>
+</Container>
+<Container>
+  <Row>
+    <Col>
+      <p>
+        Es un framework reactivo y basado en componentes que utiliza un paso de
+        compilación al momento de realizar cambios en el DOM (Document Object
+        Model).
       </p>
     </Col>
   </Row>
@@ -89,37 +133,79 @@ let imgan = '/img/reactp.png'
   <Row>
     <Col>
       <p>
-        La clave de un framework no es tanto que te ofrezca una cantidad enorme
-        de funcionalidad ya lista, con implementaciones de piezas comunes en
-        aplicaciones web, sino que están pensados para ofrecer al equipo de
-        trabajo un esquema de desarrollo sólido. Los frameworks están pensados
-        para obligar al desarrollador, en mayor o menor medida, a trabajar de un
-        modo específico, que está probado facilita la creación de aplicaciones
-        complejas, con un código fácilmente mantenible.
+        Tal vez digas, reactivo?, con que se come eso? bueno, un framework
+        reactivo significa que utiliza conceptos de la programación reactiva
+        para realizar los cambios correspondiente en el estado de nuestras
+        aplicaciones.
+      </p>
+    </Col>
+  </Row>
+  <Row>
+    <Col>
+      <p>
+        Pero y ¿qué es programación reactiva? pues de manera corta y precisa, es
+        un tipo de programación que trabaja con el flujo de datos (streams) de
+        forma asíncrona y que reacciona según el cambio de datos.
+      </p>
+    </Col>
+  </Row>
+  <Row>
+    <Col>
+      <p>
+        Un ejemplo, serían las hojas de cálculo en excel donde si pones una
+        fórmula en una celda se pone a la espera a que el usuario introduzca los
+        valores (asincronía) correspondientes para realizar el cálculo total e
+        imprimir el resultado en pantalla (streams) y si modificas alguna celda,
+        cambia el resultado total según ese nuevo dato sin ejecutar la operación
+        en toda la tabla, solo donde se necesitó un cambio.
       </p>
     </Col>
   </Row>
 
+</Container>
+<Container>
+  <h1 class="titulo1" align="center">
+    <strong>Caracteristicas de Svelte?</strong>
+  </h1>
+</Container>
+<Container>
   <Row>
     <Col>
-      <p>
-        En Javascript el uso de frameworks está ampliamente extendido y todos
-        ellos ofrecen a los desarrolladores la posibilidad de trabajo con vistas
-        (capa de presentación) a las cuales introducir fácilmente valores que
-        vienen de la capa de datos. Mediante las vistas es fácil y práctico
-        generar el HTML del lado del cliente, con datos puros que se entregan
-        del lado del servidor.
-      </p>
+      <ol>
+        <li>
+          No utiliza virtual DOM para realizar cambios en la aplicación como
+          React o Vue: Este paso se basa en compilar nuestros componentes cuando
+          realizamos un cambio, quitándole el trabajo al navegador de estar
+          guardando en memoria nuestro árbol de elementos para hacer una
+          comparación de un cambio que se hizo y solo se encarga de renderizar.
+        </li>
+        <li>
+          Tiene su propio manejador de estado (Stores): Para los que sufren con
+          entender Redux o otras librerías, Svelte ya trae su propio manejador y
+          bastante más sencillo de aprender.
+        </li>
+        <li>
+          Su sintaxis es sencilla: Esto ayuda al desarrollador escribir
+          componentes con los lenguajes que ya conoce como HTML, CSS, JavaScript
+          e incluso para quienes buscan aprender su primer framework JavaScript
+          esta sería su oportunidad de darle un vistazo.
+        </li>
+        <li>
+          Totalmente reactivo: Ayuda en la escalabilidad de nuestros proyectos
+          para que sean más mantenibles y robustos.
+        </li>
+        <li>
+          El tamaño de las apps que compilas a producción son bien reducidos a
+          diferencia de otras alternativas y con código optimizado para
+          producción**.**
+        </li>
+        <li>La documentación es super mega fácil de seguir paso a paso.</li>
+        <li>
+          Puedes utilizarlo para el server side rendering (SSR) con Sapper
+          (propió de Svelte)
+        </li>
+      </ol>
     </Col>
   </Row>
 
-  <Row>
-    <Col>
-      <p>
-        una encuesta realizada por statesofjs en el 2019 muestra el estado de
-        los frameworks que actualmente marcan lo que el mercado necesita
-        <a  href="https://2019.stateofjs.com/" target="_blank">encuesta</a>.
-      </p>
-    </Col>
-  </Row>
 </Container>
